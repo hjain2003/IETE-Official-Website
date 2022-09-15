44819787d6f7b087983df73237f82845b5e3d55b
Harsh <harshjainn2003@gmail.com>
2022-09-15 23:31:48 +0530

    added

---
+++ demo.js
@@ -0,0 +1,24 @@
+//Team Slider
+
+       
+$('#customers-teams').owlCarousel({
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 0,
+    autoplay: true,
+    dots:true,
+    autoplayTimeout: 4500,
+              checkVisibility: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      1170: {
+        items: 3
+      }
+    }
+});
